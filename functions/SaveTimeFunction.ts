8c78be572950cfeec0108f18ced15e893fb81593
justin-hong22 <hong.justin6@gmail.com>
2025-02-18 09:14:08 +0900

    abstracting the holiday function

--- functions/SaveTimeFunction.ts
+++ functions/SaveTimeFunction.ts
@@ -1,5 +1,6 @@
 import { DefineFunction, Schema, SlackFunction } from "deno-slack-sdk/mod.ts";
 import { TIMECARD_DATASTORE } from "../datastores/TimecardDatastore.ts";
+import { FindHoliday } from "./FindHolidayFunction.ts";
 
 export const SaveTimeFunction = DefineFunction({
   callback_id: "save_time",
@@ -44,7 +45,7 @@ export default SlackFunction(SaveTimeFunction, async({inputs, client}) => {
   const date_in = new Date(Number(time_in) * 1000);
   const date_out = new Date(end_time);
   const duration = lunch_break ? (date_out.getTime() - date_in.getTime() - 3600000) / 3600000 : (date_out.getTime() - date_in.getTime()) / 3600000;
-  const holiday_name = findHoliday(date_in);
+  const holiday_name = FindHoliday(date_in);
 
   const uuid = crypto.randomUUID();
   const putResponse = await client.apps.datastore.put({
@@ -68,46 +69,3 @@ export default SlackFunction(SaveTimeFunction, async({inputs, client}) => {
 
   return { outputs: {} };
 });
-
-function findHoliday(date : Date)
-{
-  function getVariableHolidays(year : number, month : number, week : number, weekday : number)
-  {
-    const firstDay = new Date(year, month, 1).getDay();
-    const offset = (weekday - firstDay + 7) % 7;
-    const day = 1 + offset + (week - 1) * 7;
-    const date = (month + 1) + "-" + day;
-    return date;
-  }
-
-  const holidays = new Map([
-    ['1-1', 'New Year\'s Day'],
-    ['2-11', 'National Foundation Day'],
-    ['2-23', 'Emperor\'s Birthday'],
-    ['4-29', 'Showa Day'],
-    ['5-3', 'Constitution Day'],
-    ['5-4', 'Greenery Day'],
-    ['5-5', 'Children\'s Day'],
-    ['8-11', 'Mountain Day'],
-    ['11-3', 'Culture Day'],
-    ['11-23', 'Labor Thanksgiving Day'],
-  ]);
-
-  const input_date =  new Date(date.toLocaleString("en-US", { timeZone: "Asia/Tokyo" }));
-  const input_year = input_date.getFullYear();
-  holidays.set(getVariableHolidays(input_year, 0, 2, 1), 'Coming of Age Day');
-  holidays.set(getVariableHolidays(input_year, 6, 3, 1), 'Marine Day');
-  holidays.set(getVariableHolidays(input_year, 8, 3, 1), 'Respected for the Aged Day');
-  holidays.set(getVariableHolidays(input_year, 9, 2, 1), 'Sports Day');
-
-  const check_date = (input_date.getMonth() + 1) + '-' + input_date.getDate();
-  let holiday_name = holidays.get(check_date);
-
-  //Check for any "observed" holidays
-  if(input_date.getDay() == 1 && holiday_name == null) {
-    holiday_name = holidays.get((input_date.getMonth() + 1) + '-' + (input_date.getDate() - 1));
-    if (holiday_name != null) { holiday_name = holiday_name + " (observed)"; } 
-  }
-  
-  return holiday_name;  
-}
