3bede384ca76a3f4c21f6d8bfdbc6d0d9e17f6e3
justin-hong22 <hong.justin6@gmail.com>
2025-03-10 09:15:42 +0900

    Japanese translation added for daily reminder

--- workflows/DailyReminderWorkflow.ts
+++ workflows/DailyReminderWorkflow.ts
@@ -14,7 +14,7 @@ const DailyReminderWorkflow = DefineWorkflow({
 
 DailyReminderWorkflow.addStep(Schema.slack.functions.SendMessage, {
   channel_id: DailyReminderWorkflow.inputs.channel,
-  message: `<!channel> Good Morning! Please remember to input your clock in time today!\n\n
+  message: `<!channel> おはようございます。出勤時間を入力してください！ (Good Morning! Please remember to input your clock in time today!)\n
   <https://slack.com/shortcuts/Ft08CXRLAPB8/20f88ae541e72c3513549e7dafdf27f1>`
 });
 
